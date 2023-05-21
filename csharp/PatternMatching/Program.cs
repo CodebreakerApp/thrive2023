4d64e0f7e84edf517f872e67fd58a4e973d38e69
Christian Nagel <christian@christiannagel.com>
2023-05-21 08:25:51 -0700

    list pattern matching with benchmark

---
+++ csharp/PatternMatching/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using PatternMatching;
+
+var summary = BenchmarkRunner.Run<Runner>();
+
+//if (args.Length == 0)
+//{
+//	Console.WriteLine("Usage: dotnet run -- int|foreach|array|span");
+//    return;
+//}
+
+//if (args[0] == "int")
+//{
+//	Accumulate(data);
+//}
+
+
---
+++ csharp/PatternMatching/Runner.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+using BenchmarkDotNet.Attributes;
+
+using Microsoft.Diagnostics.Tracing.Parsers.ClrPrivate;
+
+namespace PatternMatching;
+public class Runner
+{
+    private int[] _data = { 1, 2, 3, 5, 6, 7, 8, 9, 11, 22 };
+
+
+    [Benchmark]
+    public void IntWithFor()
+    {
+        var result = Accumulate1(_data);
+    }
+
+    [Benchmark]
+    public void IntWithForEach()
+    {
+        var result = Accumulate2(_data);
+    }
+
+    [Benchmark]
+    public void GenericWithForeach()
+    {
+        var result = Accumulate3(_data);
+    }
+
+    [Benchmark]
+    public void PatternMatching()
+    {
+        var result = Accumulate4(_data);
+    }
+
+    [Benchmark]
+    public void PatternMatchingWithSpan()
+    {
+        var result = Accumulate5(_data.AsSpan());
+    }
+
+    static public int Accumulate1(int[] data)
+    {
+        int sum = 0;
+        for (int i = 0; i < data.Length; i++) 
+        {
+            sum += data[i];
+        }
+        return sum;
+    }
+
+    static public int Accumulate2(int[] data)
+    {
+        int sum = 0;
+        foreach (var item in data)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+
+    static public T Accumulate3<T>(T[] data)
+        where T : INumberBase<T>
+    {
+        T sum = T.Zero;
+        foreach (T item in data)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+
+    static public T Accumulate4<T>(T[] data)
+        where T : INumberBase<T>
+    {
+        return data switch
+        {
+            [] => T.Zero,
+            [T item, .. var rest] => item + Accumulate4(rest)
+        };
+    }
+
+    static public T Accumulate5<T>(Span<T> data)
+    where T : INumberBase<T>
+    {
+        return data switch
+        {
+            [] => T.Zero,
+            [T item, .. var rest] => item + Accumulate5(rest)
+        };
+    }
+}
