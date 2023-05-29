5013eba61d21ca26fd67cac77257cff758404bcb
Christian Nagel <christian@christiannagel.com>
2023-05-29 22:03:19 +0200

    parse sample

---
+++ csharp/03c_ParseSample/CodebreakerModels/SimpleColorResult.cs
@@ -0,0 +1,16 @@
+﻿namespace Codebreaker.GameAPIs.Models;
+
+public enum ResultValue : byte
+{
+    Incorrect = 0,
+    CorrectColor = 1,
+    CorrectPositionAndColor = 2
+}
+
+public readonly partial struct SimpleColorResult
+{
+    private const char Separator = ':';
+    private readonly ResultValue[] _results;
+    public SimpleColorResult(ResultValue[] results) =>
+        _results = results;
+}
---
+++ csharp/03c_ParseSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using Codebreaker.GameAPIs.Models;
+
+using ParseSample;
+
+ReadOnlySpan<char> name = "Andreas Nikolaus Lauda".AsSpan();
+if (Person.TryParse(name, null, out Person? p))
+{
+    Console.WriteLine(p.FirstName);
+    Console.WriteLine(p.MiddleName);
+    Console.WriteLine(p.LastName);
+    Console.WriteLine(p);
+}
+
+QueryParam<int> qp1 = new("42", 0);
+Console.WriteLine(qp1.Value);
+QueryParam<Person> qp2 = new("Bruce Wayne", Person.Empty);
+Person p2 = qp2.Value;
+Console.WriteLine(p2);
+
+var dataSpan = new char[64].AsSpan();
+if (p2.TryFormat(dataSpan, out int chars, "F".AsSpan()))
+{
+    var first = dataSpan[..chars];
+    Console.WriteLine(first.ToString());
+}
+
+
+if (ColorResult.TryParse("1:2", default, out ColorResult result))
+{
+    Console.WriteLine($"correct: {result.Correct}, wrong position: {result.WrongPosition}");
+}
+
+if (ShapeAndColorResult.TryParse("1:2:3", default, out ShapeAndColorResult result2))
+{
+    Console.WriteLine($"correct: {result2.Correct}, wrong position: {result2.WrongPosition}, color or shape: {result2.ColorOrShape}");
+}
