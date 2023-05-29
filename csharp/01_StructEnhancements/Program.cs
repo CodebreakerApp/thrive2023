87a42e41fd105fb577364b8d4b43f93ae3890632
Christian Nagel <christian@christiannagel.com>
2023-05-29 21:10:33 +0200

    struct enhancements

---
+++ csharp/01_StructEnhancements/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// readonly struct records are immutable (get and init accessor)
+MyRecord1 r1 = new();
+
+// struct records are mutable (get and set accessor)
+MyRecord2 r2 = new();
+
+// this is just a struct, but you can use the **with** expression!
+MyData mydata1 = new();
+MyData mydata2 = mydata1 with { Z = 42 };
+Console.WriteLine(r1);
+Console.WriteLine(r2);
+Console.WriteLine(mydata1);
+Console.WriteLine(mydata2);
+
+// positional readonly struct records
+public readonly record struct MyRecord1(int X, int Y);
+
+public record struct MyRecord2(int X, int Y);
+
+// parameterless constructor with structs!
+public struct MyData
+{
+    public MyData()
+    {
+        X = -1;
+        Y = -1;
+        Z = 0; // remove with C# 11
+    }
+
+    public int X;
+    public int Y;
+    public int Z;
+
+    public readonly override string ToString() => $"X: {X}, Y: {Y}, Z: {Z}";
+}
