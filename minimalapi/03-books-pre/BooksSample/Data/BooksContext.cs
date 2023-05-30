613b2460c0424749db4fbc44cc6042940bdbddba
Christian Nagel <christian@christiannagel.com>
2023-05-30 22:30:47 +0200

    books with ef core

---
+++ minimalapi/03-books-pre/BooksSample/Data/BooksContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksSample.Data
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext(DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books => Set<Book>();
+    }
+}
