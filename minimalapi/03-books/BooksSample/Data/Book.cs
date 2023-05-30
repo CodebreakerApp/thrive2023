2e34746e6037d6d1093346805d62a2aeb7768fa2
Christian Nagel <christian@christiannagel.com>
2023-05-30 22:30:58 +0200

    books api

---
+++ minimalapi/03-books/BooksSample/Data/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace BooksSample.Data;
+
+public class Book
+{
+    public int BookId { get; set; }
+    public required string Title { get; set; }
+    public string? Publisher { get; set; }
+}
---
+++ minimalapi/03-books/BooksSample/Data/BooksContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksSample.Data;
+
+public class BooksContext : DbContext
+{
+    public BooksContext(DbContextOptions<BooksContext> options)
+        : base(options)
+    {            
+    }
+
+    public DbSet<Book> Books => Set<Book>();
+}
